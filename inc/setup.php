4fd4d9892e5d666d55ffbc1a2c01f7439a35a64f
StyleWp <stylewpnet@gmail.com>
2020-05-14 09:10:19 +0200

    Update setup.php

--- inc/setup.php
+++ inc/setup.php
@@ -49,7 +49,7 @@ if ( ! function_exists( 'stylewp_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
-				'menu-1' => esc_html__( 'Primary', 'stylewp' ),
+				'primary' => esc_html__( 'Primary', 'stylewp' ),
 			)
 		);
 
